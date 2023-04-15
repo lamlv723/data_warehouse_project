13b078e20e62573b0bf1963e36ed03a3e714ffd8
Lam <lamlv723@gmail.com>
2023-04-15 23:36:13 +0700

    0101 - Select 3 cols and rename

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,3 +1,11 @@
+-- 0100
+-- SELECT 
+--   *
+-- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+
+---0101
 SELECT 
-  *
+  order_line_id AS sales_order_line_key
+  , quantity	AS quantity
+  , unit_price	AS unit_price
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
