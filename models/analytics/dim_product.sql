3e8af27b51b47e78f767a1a0b066ea37576dc95f
Lam <lamlv723@gmail.com>
2023-04-16 16:08:54 +0700

    lesson 0103a

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,3 +1,11 @@
+-- Original
+-- SELECT 
+--   *
+-- FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+
+-- Lesson 0103a: Dim table
 SELECT 
-  *
+ stock_item_id AS product_key  
+ , stock_item_name AS product_name 
+ , brand AS brand_name   
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
