c64df5f5853ba88c254b09c77dcabea358ac9f4a
Lam <lamlv723@gmail.com>
2023-04-16 23:56:06 +0700

    Lesson-0105: Create data layer

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,11 +1,46 @@
+-- Lesson-0105: Create data layer
+WITH fact_sales_order_line__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line__rename_column AS (
+  SELECT
+    order_line_id AS sales_order_line_key
+    , stock_item_id AS product_key
+    , quantity AS quantity
+    , unit_price AS unit_price
+    , quantity * unit_price AS gross_amount
+  FROM fact_sales_order_line__source
+)
+
+, fact_sales_order_line__cast_type AS (
+  SELECT
+    CAST ( sales_order_line_key AS INTEGER ) AS sales_order_line_key
+    , CAST ( product_key AS INTEGER ) AS product_key
+    , CAST ( quantity AS INTEGER ) AS quantity
+    , CAST ( unit_price AS NUMERIC ) AS unit_price
+  FROM fact_sales_order_line__rename_column
+)
+
+SELECT
+  sales_order_line_key
+  , product_key
+  , quantity
+  , unit_price
+  , quantity * unit_price AS gross_amount
+FROM fact_sales_order_line__cast_type
+
+------------------------------------------------------------
+
 -- Lesson-0104b: Cast data type
-SELECT 
-  CAST ( order_line_id AS INTEGER ) AS sales_order_line_key
-  , CAST ( stock_item_id AS INTEGER ) AS product_key
-  , CAST ( quantity AS INTEGER ) AS quantity
-  , CAST ( unit_price AS NUMERIC ) AS unit_price
-  , CAST ( quantity AS INTEGER ) * CAST ( unit_price AS NUMERIC ) AS gross_amount
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+-- SELECT
+--   CAST ( order_line_id AS INTEGER ) AS sales_order_line_key
+--   , CAST ( stock_item_id AS INTEGER ) AS product_key
+--   , CAST ( quantity AS INTEGER ) AS quantity
+--   , CAST ( unit_price AS NUMERIC ) AS unit_price
+--   , CAST ( quantity AS INTEGER ) * CAST ( unit_price AS NUMERIC ) AS gross_amount
+-- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
 
 ------------------------------------------------------------
 
