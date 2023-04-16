66ec99df28dd34f823f04ffbfae8edd6a27ea767
Lam <lamlv723@gmail.com>
2023-04-17 00:55:41 +0700

    add sales_order_key to model

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -7,6 +7,7 @@ WITH fact_sales_order_line__source AS (
 , fact_sales_order_line__rename_column AS (
   SELECT
     order_line_id AS sales_order_line_key
+    , order_id AS sales_order_key
     , stock_item_id AS product_key
     , quantity AS quantity
     , unit_price AS unit_price
@@ -16,6 +17,7 @@ WITH fact_sales_order_line__source AS (
 , fact_sales_order_line__cast_type AS (
   SELECT
     CAST ( sales_order_line_key AS INTEGER ) AS sales_order_line_key
+    , CAST ( sales_order_key AS INTEGER ) AS sales_order_key
     , CAST ( product_key AS INTEGER ) AS product_key
     , CAST ( quantity AS INTEGER ) AS quantity
     , CAST ( unit_price AS NUMERIC ) AS unit_price
@@ -31,6 +33,7 @@ WITH fact_sales_order_line__source AS (
 
 SELECT
   sales_order_line_key
+  , sales_order_key
   , product_key
   , quantity
   , unit_price
