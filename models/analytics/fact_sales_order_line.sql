b53f79a702bf6c6261e05f4fbb118d67ee437e92
Lam <lamlv723@gmail.com>
2023-04-16 15:07:59 +0700

    Add lesson comment

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,4 +1,4 @@
--- 0100
+-- Lesson 0100
 -- SELECT 
 --   *
 -- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
@@ -8,5 +8,5 @@ SELECT
   order_line_id AS sales_order_line_key
   , quantity	AS quantity
   , unit_price	AS unit_price
-  , quantity * unit_price AS gross_amount
+  , quantity * unit_price AS gross_amount --Lesson 0102: Calculated measure
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
