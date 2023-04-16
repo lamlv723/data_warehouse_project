dd161ef92566ee7ac8dab25cb9d748f1c64e2391
Lam <lamlv723@gmail.com>
2023-04-16 22:50:20 +0700

    edit cast column splitly in calculated measure

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -16,7 +16,7 @@
 SELECT 
   CAST ( order_line_id AS INTEGER ) AS sales_order_line_key
   , CAST ( stock_item_id AS INTEGER ) AS product_key
-  , CAST ( quantity AS INTEGER )	AS quantity
-  , CAST ( unit_price AS NUMERIC )	AS unit_price
-  , CAST ( quantity * unit_price AS NUMERIC ) AS gross_amount
+  , CAST ( quantity AS INTEGER ) AS quantity
+  , CAST ( unit_price AS NUMERIC ) AS unit_price
+  , CAST ( quantity AS INTEGER ) * CAST ( unit_price AS NUMERIC ) AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
