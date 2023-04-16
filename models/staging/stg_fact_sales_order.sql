10dec354e200b14cfa5244bf536ceb9facdac020
Lam <lamlv723@gmail.com>
2023-04-17 00:55:53 +0700

    add staging table

---
+++ models/staging/stg_fact_sales_order.sql
@@ -0,0 +1,24 @@
+-- Lesson-0106b: Create staging table
+WITH sales_order__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__orders`
+)
+
+, sales_order__rename_column AS (
+  SELECT
+    order_id AS sales_order_key
+    , customer_id AS customer_key
+  FROM sales_order__source
+)
+
+, sales_order__cast_type AS (
+  SELECT
+    CAST ( sales_order_key AS INTEGER ) AS sales_order_key
+    , CAST ( customer_key AS INTEGER ) AS customer_key
+  FROM sales_order__rename_column
+)
+
+SELECT
+  sales_order_key
+  , customer_key
+FROM sales_order__cast_type
