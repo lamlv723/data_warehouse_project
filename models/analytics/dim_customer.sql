b9277f2dc0261ae171e24e814b2f017a065b0b4e
Lam <lamlv723@gmail.com>
2023-04-17 00:06:56 +0700

    Lesson-0106a: get columns

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,12 @@
+-- Lesson-0106a: Get columns
 SELECT 
-  *
+  customer_id AS customer_key
+  , customer_name AS customer_name
 FROM `vit-lam-data.wide_world_importers.sales__customers`
+
+------------------------------------------------------------
+
+-- Original
+-- SELECT 
+--   *
+-- FROM `vit-lam-data.wide_world_importers.sales__customers`
