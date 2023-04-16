8e034c41dd44b71579a99b2bb3373719ad28dadb
Lam <lamlv723@gmail.com>
2023-04-16 22:47:01 +0700

    Lesson-0104: Cast data type

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -3,14 +3,14 @@
 --   *
 -- FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 
--- Lesson 0103a: Dim table
+-- Lesson-0103a: Dim table
 -- SELECT 
 --  stock_item_id AS product_key  
 --  , stock_item_name AS product_name 
 --  , brand AS brand_name   
 -- FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 
--- Lesson 0104a: Cast data type
+-- Lesson-0104a: Cast data type
 SELECT 
  CAST ( stock_item_id AS INTEGER ) AS product_key  
  , CAST ( stock_item_name AS STRING ) AS product_name 
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,13 +1,22 @@
--- Lesson 0100
+-- Original
 -- SELECT 
 --   *
 -- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
 
----0101
+--- Lesson-0101
+-- SELECT 
+--   order_line_id AS sales_order_line_key
+--   , quantity	AS quantity
+--   , unit_price	AS unit_price
+--   , quantity * unit_price AS gross_amount --Lesson 0102: Calculated measure
+--   , stock_item_id AS product_key --Lesson 0103b
+-- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+
+-- Lesson-0104b: Cast data type
 SELECT 
-  order_line_id AS sales_order_line_key
-  , quantity	AS quantity
-  , unit_price	AS unit_price
-  , quantity * unit_price AS gross_amount --Lesson 0102: Calculated measure
-  , stock_item_id AS product_key --Lesson 0103b
+  CAST ( order_line_id AS INTEGER ) AS sales_order_line_key
+  , CAST ( stock_item_id AS INTEGER ) AS product_key
+  , CAST ( quantity AS INTEGER )	AS quantity
+  , CAST ( unit_price AS NUMERIC )	AS unit_price
+  , CAST ( quantity * unit_price AS NUMERIC ) AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
