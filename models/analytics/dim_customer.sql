fc098e4a74c29ff440ca6255cd9ceb1982a73697
Lam <lamlv723@gmail.com>
2023-04-18 01:59:57 +0700

    add customer_category_key and buying_group_key

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,6 +7,9 @@ WITH dim_customer__source AS (
   SELECT
     customer_id AS customer_key
     , customer_name AS customer_name
+    -- Lesson-0107c: Flatten dim_customer
+    , customer_category_id AS customer_category_key
+    , buying_group_id AS buying_group_key
   FROM dim_customer__source
 )
 
@@ -14,12 +17,16 @@ WITH dim_customer__source AS (
   SELECT
     CAST ( customer_key AS INTEGER) AS customer_key
     , CAST ( customer_name AS STRING) AS customer_name
+    , CAST ( customer_category_key AS INTEGER ) AS customer_category_key
+    , CAST ( buying_group_key AS INTEGER ) AS buying_group_key
   FROM dim_customer__rename_column
 )
 
 SELECT
   customer_key
   , customer_name
+  , customer_category_key
+  , buying_group_key
 FROM dim_customer__cast_type
 
 ------------------------------------------------------------
