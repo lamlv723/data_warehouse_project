1d09d954056102e363e5bf5108931b416053e017
Lam <lamlv723@gmail.com>
2023-04-18 01:44:41 +0700

    Lesson0107b: Flatten dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,6 +7,7 @@ WITH dim_product__source AS (
 , dim_product__rename_column AS (
   SELECT
     stock_item_id AS product_key
+    , supplier_id AS supplier_key
     , stock_item_name AS product_name
     , brand AS brand_name
   FROM dim_product__source
@@ -17,14 +18,20 @@ WITH dim_product__source AS (
     CAST ( product_key AS INTEGER ) AS product_key
     , CAST ( product_name AS STRING ) AS product_name
     , CAST ( brand_name AS STRING ) AS brand_name
+    , CAST ( supplier_key AS INTEGER ) AS supplier_key
   FROM dim_product__rename_column
 )
 
 SELECT
-  product_key
-  , product_name
-  , brand_name
-FROM dim_product__cast_type
+  dim_product.product_key
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_product.supplier_key
+  , dim_supplier.supplier_name
+FROM dim_product__cast_type AS dim_product
+LEFT JOIN {{ ref ('dim_supplier') }} AS dim_supplier
+ON dim_product.supplier_key = dim_supplier.supplier_key
+
 
 ------------------------------------------------------------
 
