8ebfb3bdfc8c4803b46ff93cfeab4e9caa7704c9
Lam <lamlv723@gmail.com>
2023-04-18 08:28:03 +0700

    minor adjustment

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -33,12 +33,12 @@ WITH fact_sales_order_line__source AS (
 
 SELECT
   fact_line.sales_order_line_key
-  , fact_line.sales_order_key
   , fact_line.product_key
-  , fact_header.customer_key -- Processed in stg model
   , fact_line.quantity
   , fact_line.unit_price
   , fact_line.gross_amount
+  , fact_line.sales_order_key
+  , fact_header.customer_key -- Processed in stg model
 FROM fact_sales_order_line__calculated_measure_gross_amount AS fact_line
 -- Lesson-0106b: dbt ref
 LEFT JOIN {{ ref ('stg_fact_sales_order') }} AS fact_header 
