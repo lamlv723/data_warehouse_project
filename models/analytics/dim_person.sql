d0477e38f21fcabc5430d2d6096917f59ac1327f
Lam <lamlv723@gmail.com>
2023-04-19 15:50:54 +0700

    add dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,23 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+WITH dim_person__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename_column AS (
+  SELECT
+    person_id AS person_key
+    , full_name AS full_name
+  FROM dim_person__source
+)
+
+, dim_person__cast_type AS (
+  SELECT
+    CAST ( person_key AS INTEGER ) AS person_key
+    , CAST ( full_name AS STRING ) AS full_name
+  FROM dim_person__rename_column
+)
+
+SELECT
+  person_key
+  , full_name
+FROM dim_person__cast_type
