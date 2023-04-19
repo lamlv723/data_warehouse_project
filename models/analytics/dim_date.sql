2b62530e035bba1740351e698e99cf8f63d63302
Lam <lamlv723@gmail.com>
2023-04-20 01:58:31 +0700

    adjust code to make it cleaner, follow course rule

--- models/analytics/dim_date.sql
+++ models/analytics/dim_date.sql
@@ -4,16 +4,36 @@ WITH dim_date__generate_date AS (
     UNNEST(GENERATE_DATE_ARRAY('2010-01-01', '2030-12-31', INTERVAL 1 DAY)) AS date
 )
 
-SELECT
-  date,
-  FORMAT_DATE('%A', date) AS day_of_week -- Get weekday name 
-  , FORMAT_DATE('%a', date) AS day_of_week_short -- Get weekday name short 
+, dim_date__add_custom_column AS (
+  SELECT
+    *
+    , FORMAT_DATE('%A', date) AS day_of_week -- Get weekday name 
+    , FORMAT_DATE('%a', date) AS day_of_week_short -- Get weekday name short 
+    , DATE_TRUNC (date, MONTH) AS year_month
+    , FORMAT_DATE('%B', date) AS month --Get month name
+    , DATE_TRUNC (date, YEAR) AS year
+    , EXTRACT(YEAR FROM date) AS year_number
+  FROM dim_date__generate_date
+)
+
+, dim_date__check_weekend AS (
+  SELECT
+  *
   , CASE 
-      WHEN FORMAT_DATE('%A', date) IN ('Sunday', 'Saturday') THEN 'Weekend'
-      ELSE 'Weekday'
-    END AS day_is_weekday
-  , DATE_TRUNC (date, MONTH) AS year_month
-  , FORMAT_DATE('%B', date) AS month --Get month name
-  , DATE_TRUNC (date, YEAR) AS year
-  , EXTRACT(YEAR FROM date) AS year_number
-FROM dim_date__generate_date
+      WHEN day_of_week_short IN ('Mon', 'Tue', 'Wed', 'Thu', 'Fri') THEN 'Weekday'
+      WHEN day_of_week_short IN ('Sat', 'Sun') THEN 'Weekend'
+      ELSE 'Invalid'
+    END AS is_weekday_or_weekend
+  FROM dim_date__add_custom_column
+)
+
+SELECT
+  date
+  , day_of_week
+  , day_of_week_short
+  , is_weekday_or_weekend
+  , year_month
+  , month
+  , year
+  , year_number
+FROM dim_date__check_weekend
