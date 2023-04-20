388ed710922a9828c79072196d6453ff6106fbdd
Lam <lamlv723@gmail.com>
2023-04-20 15:45:09 +0700

    adjust code convention

--- models/analytics/dim_date.sql
+++ models/analytics/dim_date.sql
@@ -1,18 +1,18 @@
 WITH dim_date__generate_date AS (
   SELECT *
   FROM
-    UNNEST(GENERATE_DATE_ARRAY('2010-01-01', '2030-12-31', INTERVAL 1 DAY)) AS date
+    UNNEST ( GENERATE_DATE_ARRAY ( '2010-01-01', '2030-12-31', INTERVAL 1 DAY ) ) AS date
 )
 
 , dim_date__add_custom_column AS (
   SELECT
-    *
-    , FORMAT_DATE('%A', date) AS day_of_week -- Get weekday name 
-    , FORMAT_DATE('%a', date) AS day_of_week_short -- Get weekday name short 
-    , DATE_TRUNC (date, MONTH) AS year_month
-    , FORMAT_DATE('%B', date) AS month --Get month name
-    , DATE_TRUNC (date, YEAR) AS year
-    , EXTRACT(YEAR FROM date) AS year_number
+    * 
+    , FORMAT_DATE ( '%A', date ) AS day_of_week -- Get weekday name 
+    , FORMAT_DATE ( '%a', date ) AS day_of_week_short -- Get weekday name short 
+    , DATE_TRUNC  ( date, MONTH ) AS year_month
+    , FORMAT_DATE ( '%B', date ) AS month --Get month name
+    , DATE_TRUNC  ( date, YEAR ) AS year
+    , EXTRACT ( YEAR FROM date ) AS year_number
   FROM dim_date__generate_date
 )
 
