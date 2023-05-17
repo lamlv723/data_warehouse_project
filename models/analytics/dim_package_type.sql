da2c8291bbdb1a527cdc8e5b08466580fd339a58
Lam <lamlv723@gmail.com>
2023-05-17 23:11:19 +0700

    add dim_package_type to model

---
+++ models/analytics/dim_package_type.sql
@@ -0,0 +1,41 @@
+WITH dim_package_type__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__package_types`
+)
+
+, dim_package_type__rename_column AS (
+  SELECT
+    package_type_id AS package_type_key
+    , package_type_name AS package_type_name
+  FROM dim_package_type__source
+)
+
+, dim_package_type__cast_type AS (
+  SELECT
+    CAST ( package_type_key AS INTEGER ) AS package_type_key
+    , CAST ( package_type_name AS STRING ) AS package_type_name
+  FROM dim_package_type__rename_column
+)
+
+, dim_package_type_add_undefined_record AS (
+  SELECT
+    package_type_key
+    , package_type_name
+  FROM dim_package_type__cast_type
+
+  UNION ALL
+  SELECT
+    0 AS package_type_key
+    , 'Undefined' AS package_type_name
+
+  -- UNION ALL
+  -- SELECT
+  --   -1 AS package_type_key
+  --   , 'Invalid' AS package_type_name
+)
+
+SELECT
+  package_type_key
+  , package_type_name
+FROM dim_package_type_add_undefined_record
+ORDER BY package_type_key
