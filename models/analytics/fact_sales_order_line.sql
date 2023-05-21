bde0fbaed52dfa974158a582d31465f998f01609
Lam <lamlv723@gmail.com>
2023-05-21 23:15:14 +0700

    create fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -7,77 +7,58 @@ WITH fact_sales_order_line__source AS (
 , fact_sales_order_line__rename_column AS (
   SELECT
     order_line_id AS sales_order_line_key
+    , description AS description
     , order_id AS sales_order_key
     , stock_item_id AS product_key
+    , package_type_id AS package_type_key
     , quantity AS quantity
     , unit_price AS unit_price
+    , tax_rate AS tax_rate
+    , picked_quantity AS picked_quantity
   FROM fact_sales_order_line__source
 )
 
 , fact_sales_order_line__cast_type AS (
   SELECT
     CAST ( sales_order_line_key AS INTEGER ) AS sales_order_line_key
+    , CAST ( description AS STRING ) AS description
     , CAST ( sales_order_key AS INTEGER ) AS sales_order_key
     , CAST ( product_key AS INTEGER ) AS product_key
+    , CAST ( package_type_key AS INTEGER ) AS package_type_key
     , CAST ( quantity AS INTEGER ) AS quantity
     , CAST ( unit_price AS NUMERIC ) AS unit_price
+    , CAST ( tax_rate AS NUMERIC ) AS tax_rate
+    , CAST ( picked_quantity AS INTEGER ) AS picked_quantity
   FROM fact_sales_order_line__rename_column
 )
 
-, fact_sales_order_line__calculated_measure_gross_amount AS (
+, fact_sales_order_line__add_calculated_measure AS (
   SELECT
   *
   , quantity * unit_price AS gross_amount
+  , ( quantity * unit_price ) * ( tax_rate / 100 ) AS tax_amount
   FROM fact_sales_order_line__cast_type
 )
 
 SELECT
   fact_line.sales_order_line_key
+  , fact_line.description
   , fact_line.product_key
+  , fact_line.package_type_key
   , fact_line.sales_order_key
-  , COALESCE ( fact_header.customer_key, -1 ) AS customer_key -- Handiling null due to LEFT JOIN. Lesson 0110d
-  , COALESCE ( fact_header.picked_by_person_key, -1 ) AS picked_by_person_key -- Handiling null due to LEFT JOIN
-  , fact_header.order_date -- Lesson-0111a
+  , COALESCE ( fact_header.customer_key, -1 ) AS customer_key
+  , COALESCE ( fact_header.salesperson_person_key, -1 ) AS salesperson_person_key
+  , COALESCE ( fact_header.picked_by_person_key, -1 ) AS picked_by_person_key
+  , COALESCE ( fact_header.contact_person_key, -1 ) AS contact_person_key
+  , fact_header.order_date
+  , fact_header.expected_delivery_date
   , fact_line.quantity
   , fact_line.unit_price
+  , fact_line.tax_rate
+  , fact_line.picked_quantity
   , fact_line.gross_amount
-FROM fact_sales_order_line__calculated_measure_gross_amount AS fact_line
+  , fact_line.tax_amount
+FROM fact_sales_order_line__add_calculated_measure AS fact_line
 
-LEFT JOIN {{ ref ('stg_fact_sales_order') }} AS fact_header -- Lesson-0106b: dbt ref
+LEFT JOIN {{ ref ('stg_fact_sales_order') }} AS fact_header
 ON fact_line.sales_order_key = fact_header.sales_order_key
-
-------------------------------------------------------------
-
--- Lesson-0106b: JOIN staging table
--- .....
--- LEFT JOIN `dbt-project-382304.wide_world_importers_dwh_staging.stg_fact_sales_order` AS fact_header
--- USING (sales_order_key)
-
-------------------------------------------------------------
-
--- Lesson-0104b: Cast data type
--- SELECT
---   CAST ( order_line_id AS INTEGER ) AS sales_order_line_key
---   , CAST ( stock_item_id AS INTEGER ) AS product_key
---   , CAST ( quantity AS INTEGER ) AS quantity
---   , CAST ( unit_price AS NUMERIC ) AS unit_price
---   , CAST ( quantity AS INTEGER ) * CAST ( unit_price AS NUMERIC ) AS gross_amount
--- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
-
-------------------------------------------------------------
-
---- Lesson-0101
--- SELECT 
---   order_line_id AS sales_order_line_key
---   , quantity	AS quantity
---   , unit_price	AS unit_price
---   , quantity * unit_price AS gross_amount --Lesson 0102: Calculated measure
---   , stock_item_id AS product_key --Lesson 0103b
--- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
-
-------------------------------------------------------------
-
--- Original
--- SELECT 
---   *
--- FROM `vit-lam-data.wide_world_importers.sales__order_lines`
--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -8,8 +8,11 @@ WITH sales_order__source AS (
   SELECT
     order_id AS sales_order_key
     , customer_id AS customer_key
+    , salesperson_person_id AS salesperson_person_key
     , picked_by_person_id AS picked_by_person_key
+    , contact_person_id AS contact_person_key
     , order_date AS order_date
+    , expected_delivery_date AS expected_delivery_date
   FROM sales_order__source
 )
 
@@ -17,14 +20,20 @@ WITH sales_order__source AS (
   SELECT
     CAST ( sales_order_key AS INTEGER ) AS sales_order_key
     , CAST ( customer_key AS INTEGER ) AS customer_key
+    , CAST ( salesperson_person_key AS INTEGER ) AS salesperson_person_key
     , CAST ( picked_by_person_key AS INTEGER ) AS picked_by_person_key
+    , CAST ( contact_person_key AS INTEGER ) AS contact_person_key
     , CAST ( order_date AS DATE ) AS order_date
+    , CAST ( expected_delivery_date AS DATE ) AS expected_delivery_date
   FROM sales_order__rename_column
 )
 
 SELECT
   sales_order_key
   , COALESCE ( customer_key, 0 ) AS customer_key
+  , COALESCE ( salesperson_person_key, 0 ) AS salesperson_person_key
   , COALESCE ( picked_by_person_key, 0 ) AS picked_by_person_key
+  , COALESCE ( contact_person_key, 0 ) AS contact_person_key
   , order_date -- Lesson-0111a
+  , expected_delivery_date
 FROM sales_order__cast_type
