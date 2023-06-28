a5d509cd61796920cdb9985e1d2e4433c3d8c0eb
Lam <lamlv723@gmail.com>
2023-06-28 23:20:08 +0700

    create target and actual CTE

--- models/analytics/fact_target_salesperson.sql
+++ models/analytics/fact_target_salesperson.sql
@@ -1,26 +1,16 @@
-WITH fact_target_salesperson__source AS (
-  SELECT *
-  FROM `vit-lam-data.wide_world_importers.external__salesperson_target`
-)
-
-, fact_target_salesperson__rename_column AS (
+WITH fact_target_salesperson__target_source AS (
   SELECT
-    year_month AS year_month
-    , salesperson_person_id AS salesperson_person_key
-    , target_revenue AS target_gross_amount
-  FROM fact_target_salesperson__source
+    year_month
+    , salesperson_person_key
+    , target_gross_amount
+  FROM {{ref('stg_fact_target_salesperson')}}
 )
 
-, fact_target_salesperson__cast_type AS (
+, fact_target_salesperson__acctual_source AS (
   SELECT
-    CAST ( year_month AS DATE ) AS year_month
-    , CAST ( salesperson_person_key AS INTEGER ) AS salesperson_person_key
-    , CAST ( target_gross_amount AS NUMERIC ) AS target_gross_amount
-  FROM fact_target_salesperson__rename_column
+    DATE_TRUNC(order_date, MONTH) AS year_month
+    , salesperson_person_key
+    , SUM(gross_amount) AS gross_amount
+  FROM {{ref('fact_sales_order_line')}}
+  GROUP BY 1, 2
 )
-
-SELECT
-  year_month
-  , salesperson_person_key
-  , target_gross_amount
-FROM fact_target_salesperson__cast_type
