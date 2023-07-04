6ac4c356e9dd71c987724dbfb46f8f83e785b07d
Lam <lamlv723@gmail.com>
2023-07-05 00:07:32 +0700

    calculate customer segment

--- models/analytics/fact_customer_snapshot_by_month.sql
+++ models/analytics/fact_customer_snapshot_by_month.sql
@@ -17,9 +17,36 @@ WITH fact_customer_snapshot_by_month__summarize AS (
 , fact_customer_snapshot_by_month__calculate_percentile AS (
   SELECT
     *
-    , PERCENT_RANK() OVER(ORDER BY sales_amount) AS sales_amount_percentile
-    , PERCENT_RANK() OVER(ORDER BY lifetime_sales_amount) AS lifetime_sales_amount_percentile
+    , PERCENT_RANK() OVER(PARTITION BY year_month ORDER BY sales_amount) AS sales_amount_percentile
+    , PERCENT_RANK() OVER(PARTITION BY year_month ORDER BY lifetime_sales_amount) AS lifetime_sales_amount_percentile
   FROM fact_customer_snapshot_by_month__calculate_cumulative
 )
 
-SELECT * FROM fact_customer_snapshot_by_month__calculate_percentile ORDER BY 2, 1 LIMIT 1000
+, fact_customer_snapshot_by_month__percentile_segment AS (
+  SELECT
+    *
+    , CASE
+        WHEN sales_amount_percentile BETWEEN 0.8 AND 1 THEN 'High'
+        WHEN sales_amount_percentile BETWEEN 0.5 AND 0.8 THEN 'Medium'
+        WHEN sales_amount_percentile BETWEEN 0 AND 0.5 THEN 'Low'
+        ELSE 'Invalid'
+      END AS sales_amount_segment
+    , CASE
+        WHEN lifetime_sales_amount_percentile BETWEEN 0.8 AND 1 THEN 'High'
+        WHEN lifetime_sales_amount_percentile BETWEEN 0.5 AND 0.8 THEN 'Medium'
+        WHEN lifetime_sales_amount_percentile BETWEEN 0 AND 0.5 THEN 'Low'
+        ELSE 'Invalid'
+      END AS lifetime_sales_amount_segment
+  FROM fact_customer_snapshot_by_month__calculate_percentile
+)
+
+SELECT
+  year_month
+  , customer_key
+  , sales_amount
+  , lifetime_sales_amount
+  -- , sales_amount_percentile
+  -- , lifetime_sales_amount_percentile
+  , sales_amount_segment
+  , lifetime_sales_amount_segment
+FROM fact_customer_snapshot_by_month__percentile_segment
