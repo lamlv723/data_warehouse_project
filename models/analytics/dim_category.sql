4cf8f83562d1edf97f52c88627528813c05856c1
lam <lamlv723@gmail.com>
2023-07-27 22:43:10 +0700

    add dim_category table

---
+++ models/analytics/dim_category.sql
@@ -0,0 +1,52 @@
+WITH dim_category__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.external__categories`
+)
+
+, dim_category__rename_column AS (
+  SELECT
+    category_id AS category_key
+    , category_name AS category_name
+    , parent_category_id AS parent_category_key
+    , category_level AS category_level
+  FROM dim_category__source
+)
+
+, dim_category__cast_type AS (
+  SELECT
+    CAST ( category_key AS INTEGER ) AS category_key
+    , CAST ( category_name AS STRING ) AS category_name
+    , CAST ( parent_category_key AS INTEGER ) AS parent_category_key
+    , CAST ( category_level AS INTEGER ) AS category_level
+  FROM dim_category__rename_column
+)
+
+, dim_category__add_undefined_record AS (
+  SELECT
+    category_key
+    , category_name
+    , parent_category_key
+    , category_level
+  FROM dim_category__cast_type
+
+  UNION ALL
+  SELECT
+    -1 AS category_key
+    , 'Invalid' AS category_name
+    , -1 AS parent_category_key
+    , -1 AS category_level
+
+  UNION ALL
+  SELECT
+    0 AS category_key
+    , 'Undefined' AS category_name
+    , 0 AS parent_category_key
+    , 0 AS category_level
+)
+
+SELECT
+  category_key
+  , category_name
+  , parent_category_key
+  , category_level
+FROM dim_category__add_undefined_record
