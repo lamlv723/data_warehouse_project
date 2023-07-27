16d6fff9e557ea9621687750b3b8852394f3ffe0
lam <lamlv723@gmail.com>
2023-07-27 16:45:05 +0700

    change column name from L12MTD* to last_12months*

--- models/analytics/dim_customer_attribute.sql
+++ models/analytics/dim_customer_attribute.sql
@@ -7,14 +7,14 @@ WITH dim_customer_attribute__summarize AS (
         CASE
           WHEN order_date BETWEEN DATE_TRUNC('2016-05-31', MONTH ) - INTERVAL 12 MONTH AND '2016-05-31' THEN gross_amount
         END
-      ) AS L12MTD_sales_amount
+      ) AS last_12months_sales_amount
     , COUNT(
         DISTINCT(
           CASE
             WHEN order_date BETWEEN DATE_TRUNC('2016-05-31', MONTH ) - INTERVAL 12 MONTH AND '2016-05-31' THEN sales_order_key
           END
         )
-      ) AS L12MTD_sales_order
+      ) AS last_12months_sales_order
     , DATE_TRUNC(MIN(order_date), MONTH) AS start_month
     , DATE_TRUNC(MAX(order_date), MONTH) AS end_month
   FROM {{ref('fact_sales_order_line')}}
@@ -25,7 +25,7 @@ WITH dim_customer_attribute__summarize AS (
   SELECT
     *
     , PERCENT_RANK() OVER(ORDER BY(lifetime_sales_amount)) AS lifetime_sales_amount_percentile
-    , PERCENT_RANK() OVER(ORDER BY(L12MTD_sales_amount)) AS L12MTD_sales_amount_percentile
+    , PERCENT_RANK() OVER(ORDER BY(last_12months_sales_amount)) AS last_12months_sales_amount_percentile
   FROM dim_customer_attribute__summarize
 )
 
@@ -39,11 +39,11 @@ WITH dim_customer_attribute__summarize AS (
         ELSE 'Invalid'
       END AS lifetime_sales_amount_segment
     , CASE
-        WHEN L12MTD_sales_amount_percentile BETWEEN 0.8 AND 1 THEN 'High'
-        WHEN L12MTD_sales_amount_percentile BETWEEN 0.5 AND 0.8 THEN 'Medium'
-        WHEN L12MTD_sales_amount_percentile BETWEEN 0 AND 0.5 THEN 'Low'
+        WHEN last_12months_sales_amount_percentile BETWEEN 0.8 AND 1 THEN 'High'
+        WHEN last_12months_sales_amount_percentile BETWEEN 0.5 AND 0.8 THEN 'Medium'
+        WHEN last_12months_sales_amount_percentile BETWEEN 0 AND 0.5 THEN 'Low'
         ELSE 'Invalid'
-      END AS L12MTD_sales_amount_segment
+      END AS last_12months_sales_amount_segment
   FROM dim_customer_attribute__calculate_percentile
 )
 
@@ -51,12 +51,12 @@ SELECT
   customer_key
   , lifetime_sales_amount
   , lifetime_sales_order
-  , L12MTD_sales_amount
-  , L12MTD_sales_order
+  , last_12months_sales_amount
+  , last_12months_sales_order
   , lifetime_sales_amount_percentile
-  , L12MTD_sales_amount_percentile
+  , last_12months_sales_amount_percentile
   , lifetime_sales_amount_segment
-  , L12MTD_sales_amount_segment
+  , last_12months_sales_amount_segment
   , start_month
   , end_month
 FROM dim_customer_attribute__segment_percentile
